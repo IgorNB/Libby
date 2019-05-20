38cac209f896078d5b32ee03ff67a4fb7b1bc103
iborisenko <iborisenko@akb-it.ru>
2019-05-20 03:11:27 +0300

    LoyaltyTransaction unit test

---
+++ src/test/java/com/lig/libby/domain/LoyaltyTransactionTest.java
@@ -0,0 +1,27 @@
+package com.lig.libby.domain;
+
+import com.jparams.verifier.tostring.NameStyle;
+import com.jparams.verifier.tostring.ToStringVerifier;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.jupiter.api.Test;
+
+class LoyaltyTransactionTest {
+
+    @Test
+    void equalsAndHashCode() {
+        EqualsVerifier
+                .forClass(LoyaltyTransaction.class)
+                .withPrefabValues(Task.class, new Task(), new Task())
+                .withPrefabValues(Book.class, new Book(), new Book())
+                .withPrefabValues(Work.class, new Work(), new Work())
+                .withOnlyTheseFields("id")
+                .verify();
+    }
+
+    @Test
+    void toStringTest() {
+        ToStringVerifier.forClass(LoyaltyTransaction.class)
+                .withClassName(NameStyle.SIMPLE_NAME)
+                .verify();
+    }
+}
